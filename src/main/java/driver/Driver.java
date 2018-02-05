43e374e3a1ad4cd21b55bee56988eff295d3e15b
edinc <cenanovice@gmail.com>
2018-02-05 11:00:02 +0100

    Added Driver classes

---
+++ src/main/java/driver/Driver.java
@@ -0,0 +1,36 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Driver {
+
+    WebDriver driver;
+
+    public Driver(WebDriver driver) {
+
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    private static ThreadLocal<WebDriver> webDriver = new ThreadLocal<>();
+
+    public static  WebDriver getDriver() {
+
+        return webDriver.get();
+    }
+
+    public static void setWebDriver(WebDriver driver) {
+
+        webDriver.set(driver);
+    }
+
+    protected void waitForElement(WebElement element) {
+
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        WebElement target = wait.until(ExpectedConditions.visibilityOf(element));
+    }
+}
---
+++ src/main/java/driver/DriverManager.java
@@ -0,0 +1,49 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DriverManager {
+
+    private static final String HEADLESS = "headless";
+    public static WebDriver createInstance(String browserName, String appUrl) throws MalformedURLException {
+        final String browserMode = System.getProperty("mode");
+        WebDriver driver = null;
+        if(browserName.toLowerCase().contains("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "driver/osx/geckodriver");
+            if(browserMode !=null  && browserMode.equals(HEADLESS)){
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--headless");
+                driver = new FirefoxDriver(firefoxOptions);
+            }else {
+                driver = new FirefoxDriver();
+            }
+
+        }
+        if(browserName.toLowerCase().contains("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "driver/osx/chromedriver");
+            if(browserMode !=null  && browserMode.equals(HEADLESS)){
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                driver = new ChromeDriver(chromeOptions);
+            }else {
+                driver = new ChromeDriver();
+            }
+
+        }
+        if(browserName.toLowerCase().contains("zalenium")) {
+            DesiredCapabilities cap = DesiredCapabilities.chrome();
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+        }
+        driver.navigate().to(appUrl);
+        return driver;
+    }
+}
