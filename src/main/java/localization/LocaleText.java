d401dd482ce0557bbdbf193f32effe593a085da2
edinc <cenanovice@gmail.com>
2018-02-05 11:43:12 +0100

    Added localization configuration

---
+++ src/main/java/localization/LocaleText.java
@@ -0,0 +1,27 @@
+package localization;
+
+import config.DomainConfiguration;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.Charset;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+@Component
+public class LocaleText {
+
+    private static final Locale LOCALE = DomainConfiguration.APP_DOMAIN.getLocale();
+
+
+    public static String get(final String key) {
+
+        /*************************************************************************
+         **  The resource bundle by default uses the ISO 8859-1 character encoding
+         **  Making it compatible with the commonly used UTF-8 format
+         *************************************************************************/
+
+        final String value = ResourceBundle.getBundle("locale", LOCALE).getString(key);
+        final byte[] utf8Bytes = value.getBytes(Charset.forName("ISO-8859-1"));
+        return new String(utf8Bytes, Charset.forName("UTF-8"));
+    }
+}
