7ddd9719d67438d24d39a31f3cb31c24f12ba738
edinc <cenanovice@gmail.com>
2018-02-05 11:22:57 +0100

    Added FileLoaderService class

---
+++ src/test/java/selenium/testdata/file/FileLoaderService.java
@@ -0,0 +1,43 @@
+package selenium.testdata.file;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class FileLoaderService {
+
+    private static final String TEST_DATA_FOLDER = "testdata";
+    private static final String USER_FOLDER = "user";
+
+    private final ObjectMapper objectMapper;
+    private final ResourceLoader resourceLoader;
+
+    @Autowired
+    public FileLoaderService(ObjectMapper objectMapper, ResourceLoader resourceLoader) {
+        this.objectMapper = objectMapper;
+        this.resourceLoader = resourceLoader;
+    }
+
+    public User getUser(final String fileNameWithoutCountryCodeAndExtention) {
+        return load(USER_FOLDER + "/" + fileNameWithoutCountryCodeAndExtention + "_"
+                + DomainConfiguration.APP_DOMAIN.getCountryCode() + ".json", User.class);
+    }
+
+    private <T> T load(final String filename, final Class<T> type) {
+        try{
+            final Resource resource =   resourceLoader.getResource(getFullPath(filename));
+            return objectMapper.readValue(resource.getInputStream(), type);
+        }catch (final IOException e) {
+            throw new RuntimeException("Failed to load test data", e);
+        }
+    }
+
+    private String getFullPath(final String filename) {
+        return TEST_DATA_FOLDER + "/" + filename;
+    }
+}
